7a6079088b6c590e1583c572dbd70a4928721b97
unknown <sim.karime.ahmed@alexu.edu.eg>
2021-09-01 23:18:52 +0200

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,180 @@
 @extends('layouts.app')
-
+<style>
+    .navbar{
+     visibility: hidden;}
+     .slider{
+         margin-top: -80px;
+     }
+</style>
 @section('content')
-    <div class="container">
+    <!-- <div class="container">
         <div class="row justify-content-center">
+         -->
+            <section class="slider">
+            
+            <div class=shape></div>
+            <div class=shape-01> </div>  
+            
+                <div class="banner">
+                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-md-6 col-sm-12">
+                                            <div class="wrapper">
+                                                <div class="content">
+                                                    <h1>
+                                                        YOUR DELICIOUS, FRESH FOOD
+                                                        IS IN YOUR HANDS NOW
+                                                    </h1>
+                                                    <h5>
+                                                        You can order your amazing,
+                                                        delicious, and fantastic
+                                                        food whose taste and flavor
+                                                        will make you feel happy and
+                                                        over the moon ! It's not
+                                                        about eating, It's about
+                                                        tasting
+                                                    </h5>
+                                                    <ol>
+                                                        <li>
+                                                            <a href="#">Order Now<span
+                                                                    class="flaticon-right-arrow"></span></a>
+                                                        </li>
+                                                    </ol>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-6 col-sm-12">
+                                            <div class="wrapper">
+                                                <img src="{{ asset('css/assets/images/slider/slide-01.png') }}" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-md-6 col-sm-12">
+                                            <div class="wrapper">
+                                                <div class="content">
+                                                    <h1>
+                                                        YOUR DELICIOUS, FRESH FOOD
+                                                        IS IN YOUR HANDS NOW
+                                                    </h1>
+                                                    <h5>
+                                                        You can order your amazing,
+                                                        delicious, and fantastic
+                                                        food whose taste and flavor
+                                                        will make you feel happy and
+                                                        over the moon ! It's not
+                                                        about eating, It's about
+                                                        tasting
+                                                    </h5>
+                                                    <ol>
+                                                        <li>
+                                                            <a href="#">Order Now<span
+                                                                    class="flaticon-right-arrow"></span></a>
+                                                        </li>
+                                                    </ol>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-6 col-sm-12">
+                                            <div class="wrapper">
+                                                <img src="{{ asset('css/assets/images/slider/slide-02.png') }}" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-md-6 col-sm-12">
+                                            <div class="wrapper">
+                                                <div class="content">
+                                                    <h1>
+                                                        YOUR DELICIOUS, FRESH FOOD
+                                                        IS IN YOUR HANDS NOW
+                                                    </h1>
+                                                    <h5>
+                                                        You can order your amazing,
+                                                        delicious, and fantastic
+                                                        food whose taste and flavor
+                                                        will make you feel happy and
+                                                        over the moon ! It's not
+                                                        about eating, It's about
+                                                        tasting
+                                                    </h5>
+                                                    <ol>
+                                                        <li>
+                                                            <a href="#">Order Now<span
+                                                                    class="flaticon-right-arrow"></span></a>
+                                                        </li>
+                                                    </ol>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-6 col-sm-12">
+                                            <div class="wrapper">
+                                                <img src="{{ asset('css/assets/images/slider/slide-03.png') }}" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
             <div class="col-md-8">
+                <div class="card">
+                    @empty($OrderedItems)
+                    @else
+                        <div class="card-header">Ordered Items</div>
+                        @foreach ($OrderedItems as $meal)
+                            <div class="card-body">
+                                <table>
+                                    <tr>
+                                        <th>Name:</th>
+                                        <td>{{ $meal->name }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Chef Name:</th>
+                                        <td><a href="chef/{{ $meal->chef->id }}"> {{ $meal->chef->name }}</a></td>
+                                    </tr>
+                                    <tr>
+                                        <th>Category:</th>
+                                        <td>{{ $meal->category->name }}</td>
+                                    </tr>
+                                    </tr>
+                                    <tr>
+                                        <th>Price:</th>
+                                        <td>{{ $meal->price }}</td>
+                                    </tr>
+                                    </tr>
+                                    <tr>
+                                        <th>Description:</th>
+                                        <td>{{ $meal->description }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Visit:</th>
+                                        <td><a href="/meal/{{ $meal->id }}">Click Here</a></td>
+                                    </tr>
+
+                                </table>
+                            </div>
+
+                        @endforeach
+
+                    @endempty
+                </div>
                 <div class="card">
                     <div class="card">
                         <div class="card-header">Meals</div>
@@ -54,6 +225,7 @@
 
                     </div>
                 </div>
+
                 <div class="card">
                     <div class="card-header">{{ __('Chefs') }}</div>
                     @foreach ($chefs as $chef)
